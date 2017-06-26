9f1f2490def9565af5d83a0083c8a944497baeb5
20161104615 <a20161104615@admindeiMac-38.local>
2017-06-26 09:55:33 +0800

    Update

--- GPS/main.c
+++ GPS/main.c
@@ -22,58 +22,58 @@ int main(){
         printf("文件打开失败!\n");
     }
     else {
-        for(n=0;n<k;n++){
-            fseek(f, 7+n*122L, SEEK_CUR);
-            printf("GPRMC时间为：");
-            for (i=0;i<6;i++){
-                fscanf(f,"%c",&time[i]);
-                printf("%c",time[i]);
-            }
-            fseek(f, 0L, SEEK_SET);
-            printf("\n");
-            fseek(f, 7+n*122L, SEEK_CUR);
-            fprintf(t,"%s",time);
-            fseek(f, 0L, SEEK_SET);
-            
-            
-            fseek(f, 16+n*122L, SEEK_CUR);
-            fprintf(t,"%s",time);
-            printf("GPRMC的经纬度为：");
-            for (i=0;i<22;i++){
-                fscanf(f,"%c",&DLOfGPRMCNumber[i]);
-                printf("%c",DLOfGPRMCNumber[i]);
-            }
-            fseek(f,0L,SEEK_SET);
-            printf("\n");
-            fseek(f, 16+n*122L, SEEK_CUR);
-            fprintf(t,"%s\n",DLOfGPRMCNumber);
-            fseek(f,0L,SEEK_SET);
-            
-            
-            fseek(p, 70+n*122L, SEEK_CUR);
-            printf("GPGGA时间为：");
-            for (i=0;i<6;i++){
-                fscanf(p,"%c",&time[i]);
-                printf("%c",time[i]);
-            }
-            fseek(p, 0L, SEEK_SET);
-            printf("\n");
-            fseek(p, 70+n*122L, SEEK_CUR);
-            fprintf(t,"%s",time);
-            fseek(p, 0L, SEEK_SET);
-            
-            
-            fseek(p, 77+n*122L, SEEK_CUR);
-            printf("GPGGA的经纬度为：");
-            for (i=0;i<22;i++){
-                fscanf(p,"%c",&DLOfGPGGANumber[i]);
-                printf("%c",DLOfGPGGANumber[i]);
-            }
-            fseek(p,0L,SEEK_SET);
-            printf("\n");
-            fseek(p, 78+n*122L, SEEK_CUR);
-            fprintf(t,"%s\n",DLOfGPGGANumber);
-            fseek(p,0L,SEEK_SET);
+            for(n=0;n<k;n++){
+                fseek(f, 7+n*122L, SEEK_CUR);
+                printf("GPRMC时间为：");
+                for (i=0;i<6;i++){
+                    fscanf(f,"%c",&time[i]);
+                    printf("%c",time[i]);
+                }
+                fseek(f, 0L, SEEK_SET);
+                printf("\n");
+                fseek(f, 7+n*122L, SEEK_CUR);
+                fprintf(t,"%s",time);
+                fseek(f, 0L, SEEK_SET);
+                
+                
+                fseek(f, 16+n*122L, SEEK_CUR);
+                fprintf(t,"%s",time);
+                printf("GPRMC的经纬度为：");
+                for (i=0;i<22;i++){
+                    fscanf(f,"%c",&DLOfGPRMCNumber[i]);
+                    printf("%c",DLOfGPRMCNumber[i]);
+                }
+                fseek(f,0L,SEEK_SET);
+                printf("\n");
+                fseek(f, 16+n*122L, SEEK_CUR);
+                fprintf(t,"%s\n",DLOfGPRMCNumber);
+                fseek(f,0L,SEEK_SET);
+                
+                
+                fseek(p, 70+n*122L, SEEK_CUR);
+                printf("GPGGA时间为：");
+                for (i=0;i<6;i++){
+                    fscanf(p,"%c",&time[i]);
+                    printf("%c",time[i]);
+                }
+                fseek(p, 0L, SEEK_SET);
+                printf("\n");
+                fseek(p, 70+n*122L, SEEK_CUR);
+                fprintf(t,"%s",time);
+                fseek(p, 0L, SEEK_SET);
+                
+                
+                fseek(p, 77+n*122L, SEEK_CUR);
+                printf("GPGGA的经纬度为：");
+                for (i=0;i<22;i++){
+                    fscanf(p,"%c",&DLOfGPGGANumber[i]);
+                    printf("%c",DLOfGPGGANumber[i]);
+                }
+                fseek(p,0L,SEEK_SET);
+                printf("\n");
+                fseek(p, 78+n*122L, SEEK_CUR);
+                fprintf(t,"%s\n",DLOfGPGGANumber);
+                fseek(p,0L,SEEK_SET);
         } 
         fclose(f);
         fclose(p);
